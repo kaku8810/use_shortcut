149698191732b1f8a659c0162a8d84c7d2ea909f
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2021-10-25 15:03:40 +0900

    add-text-pages #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  get 'pages/text'
+  get 'pages/text_mac'
+  get 'pages/text_mac_practice'
+  get 'pages/text_mac_test'
+  get 'pages/text_win'
+  get 'pages/text_win_practice'
+  get 'pages/text_win_test'
 
-  root 'application#hello'
 end
