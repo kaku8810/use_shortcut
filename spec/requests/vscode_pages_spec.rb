8db4c50c2873f20c4c5b8acab60573a9ca7f4267
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2021-11-01 18:28:34 +0900

    [add]vscode_pages追加

--- spec/requests/vscode_pages_spec.rb
+++ spec/requests/vscode_pages_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'VscodePages', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /vscode' do
+    it 'gets vscode' do
+      get vscode_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'gets vscode_mac' do
+      get vscode_mac_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'gets vscode_mac_practice' do
+      get vscode_mac_practice_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'gets vscode_mac_test' do
+      get vscode_mac_test_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'gets vscode_win' do
+      get vscode_win_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'gets vscode_win_practice' do
+      get vscode_win_practice_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'gets vscode_win_test' do
+      get vscode_win_test_path
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
