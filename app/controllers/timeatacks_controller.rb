aa7237a9bca83e7fb4d9111838e5e8fc3e90421f
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2021-11-09 21:49:48 +0900

    [add]timeatack_controller追加

--- app/controllers/timeatacks_controller.rb
+++ app/controllers/timeatacks_controller.rb
@@ -1,2 +1,17 @@
 class TimeatacksController < ApplicationController
+  before_action :logged_in_user, only: [:create]
+
+  def index
+    @timeatack = current_user.timeatacks.build(timeatack_params)
+    @timeatack.save
+  end
+
+  def create
+
+  end
+
+  private
+    def timeatack_params
+      params.require(:timeatack).permit(:time)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,14 +50,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "ログインしてください"
-        redirect_to login_url
-      end
-    end
-
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
